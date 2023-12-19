7a360bc055ef8b08dad2f817d9ce4d869544d59a
Jeffrey Le <jeffreyle20@gmail.com>
2023-12-19 16:04:48 -0700

    Added WorkGridItem to Works page

--- pages/works.js
+++ pages/works.js
@@ -1,5 +1,8 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+
+import icon from '../public/images/icon.png'
 
 const Works = () => {
     return (
@@ -7,6 +10,24 @@ const Works = () => {
             <Heading as="h3" fontSize={20} mb={4}>
                 Works
             </Heading>
+
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                <Section>
+                    <WorkGridItem id="title" title="Title" thumbnail={icon}>
+                        The quick brown fox jumps over the lazy dog.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem id="title" title="Title" thumbnail={icon}>
+                        The quick brown fox jumps over the lazy dog.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem id="title" title="Title" thumbnail={icon}>
+                        The quick brown fox jumps over the lazy dog.
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
         </Container>
     )
 }
