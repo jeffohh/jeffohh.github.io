e302505e7859437be7c2f20b80d7bed5ce54c48d
Jeffrey Le <jeffreyle20@gmail.com>
2023-12-19 16:19:26 -0700

    Added animation for page transition

--- pages/works.js
+++ pages/works.js
@@ -1,34 +1,37 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
 
 import icon from '../public/images/icon.png'
 
 const Works = () => {
     return (
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Works
-            </Heading>
+        <Layout>
+            <Container>
+                <Heading as="h3" fontSize={20} mb={4}>
+                    Works
+                </Heading>
 
-            <SimpleGrid columns={[1, 1, 2]} gap={6}>
-                <Section>
-                    <WorkGridItem id="title" title="Title" thumbnail={icon}>
-                        The quick brown fox jumps over the lazy dog.
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="title" title="Title" thumbnail={icon}>
-                        The quick brown fox jumps over the lazy dog.
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="title" title="Title" thumbnail={icon}>
-                        The quick brown fox jumps over the lazy dog.
-                    </WorkGridItem>
-                </Section>
-            </SimpleGrid>
-        </Container>
+                <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                    <Section>
+                        <WorkGridItem id="title" title="Title" thumbnail={icon}>
+                            The quick brown fox jumps over the lazy dog.
+                        </WorkGridItem>
+                    </Section>
+                    <Section>
+                        <WorkGridItem id="title" title="Title" thumbnail={icon}>
+                            The quick brown fox jumps over the lazy dog.
+                        </WorkGridItem>
+                    </Section>
+                    <Section>
+                        <WorkGridItem id="title" title="Title" thumbnail={icon}>
+                            The quick brown fox jumps over the lazy dog.
+                        </WorkGridItem>
+                    </Section>
+                </SimpleGrid>
+            </Container>
+        </Layout>
     )
 }
 
