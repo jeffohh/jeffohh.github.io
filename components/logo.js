ba6807a4d1a56ae4867759311936807b1adea8de
Jeffrey Le <jeffreyle20@gmail.com>
2024-01-03 21:00:04 -0700

    Commented out unused Images

--- components/logo.js
+++ components/logo.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import Image from 'next/image'
+// import Image from 'next/image'
 import { Text, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 
@@ -18,7 +18,7 @@ const LogoBox = styled.span`
 `
 
 const Logo = ( ) => {
-    const footPrintImg = `/images/${useColorModeValue('', '-dark')}.png`
+    // const footPrintImg = `/images/${useColorModeValue('', '-dark')}.png`
 
     return (
         <Link href="/">
