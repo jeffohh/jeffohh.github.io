add4ef589b37fa72d3d73673de256f90ea4240da
Jeffrey Le <jeffreyle20@gmail.com>
2024-01-03 21:25:56 -0700

    Removed PDF import

--- pages/resume.js
+++ pages/resume.js
@@ -3,9 +3,6 @@ import NextLink from 'next/link'
 import { Box, Button } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 
-import { Document, Page, pdfjs } from 'react-pdf'
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
 import pdfResume from '../public/pdfs/jle_resume-one.pdf'
 
 const Resume = ( ) => {
