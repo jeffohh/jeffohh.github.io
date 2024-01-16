237eebbbf1d2f92f6dc253aebd23272c2bacb84f
Jeffrey Le <jeffreyle20@gmail.com>
2024-01-15 17:28:14 -0700

    Added posts and socials

---
+++ pages/posts.js
@@ -0,0 +1,28 @@
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { GridItem } from '../components/grid-item'
+
+import thumbAceslinger from '../public/images/works/aceslinger.png'
+
+const Posts = () => (
+    <Layout title="Posts">
+        <Container>
+            <Heading as="h4" fontSize={20} mb={4}>
+                Posts
+            </Heading>
+            <Section delay={0.1}>
+                <SimpleGrid columns={[1, 2, 2]} gap={6}>
+                    <GridItem
+                        title="test workflow"
+                        thumbnail={thumbAceslinger}
+                    >
+
+                    </GridItem>
+                </SimpleGrid>
+            </Section>
+        </Container>
+    </Layout>
+)
+
+export default Posts
